2c493d225ce5587dd3748936765d83f999dc77f7
Christian Popp <popp.christian88@gmail.com>
2025-04-26 17:10:11 +0300

    x

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
-import './style.css'
+// src/main.js
+import './style.css';
+import 'slick-carousel/slick/slick.css'; // Import Slick CSS (you might need to adjust the path)
+import 'slick-carousel/slick/slick-theme.css'; // Import Slick theme CSS (you might need to adjust the path)
+import 'slick-carousel/slick/slick.min.js'; // This might not be strictly necessary if included via CDN
+
+$(function() {
+  $('.your-class').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    infinite: true,
+  });
+});
+
+
 
